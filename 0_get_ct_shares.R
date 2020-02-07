c60f954525861d421f4bf07b53581319fd976774
Fabio Giglietto <fabiogiglietto@users.noreply.github.com>
2020-02-07 16:13:59 +0100

    remove NULL expandedLinks
    Sometimes CrowdTangle outputs NULL values for expandedLinks. This NULL values break the following code and must be removed.

--- 0_get_ct_shares.R
+++ 0_get_ct_shares.R
@@ -54,8 +54,9 @@ for (i in 1:nrow(urls)) {
     })
 }
 
+ct_shares.df <- ct_shares.df[-which(ct_shares.df$expandedLinks == "NULL"), ] # remove shares with expandedLinks is NULL
 ct_shares.df <- unnest(ct_shares.df, ct_shares.df$expandedLinks) # unnest expanded_url list and creates original and expanded (URL) fields
-ct_shares.df <- ct_shares.df[!duplicated(ct_shares.df[,c("id", "platformId", "postUrl", "expanded")]),] # remove duplicates created by the unnesting
+ct_shares.df <- ct_shares.df[!duplicated(ct_shares.df[,c("id", "platformId", "postUrl", "expanded")]),] # remove exact duplicates created by the unnesting
 ct_shares.df$original <- NULL # remove "original" unshortnen URL
 
 saveRDS(ct_shares.df, file = "./data/ct_shares.df.rds")

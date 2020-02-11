628d43f56024ba987f902085f5c7e3c597dd1e8a
Fabio Giglietto <fabio.giglietto@gmail.com>
2020-02-11 14:51:17 +0000

    moved API token to .Renviron file

--- 0_get_ct_shares.R
+++ 0_get_ct_shares.R
@@ -12,7 +12,7 @@ dir.create("data", showWarnings = F)
 urls <- readRDS("./rawdata/urls.rds")
 
 # Get my token
-api_key <- URLencode("") # add api token here
+api_key <- Sys.getenv("CT_API_TOKEN") # add api token to your .Renviron file and restart R
 
 ct_shares.df <- NULL
 
